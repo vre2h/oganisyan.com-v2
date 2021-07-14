6abc7643c141a094941c5b6e940a0d74417ec2a6
Oganisyan Vrezh <hovanisyan.vrezh@gmail.com>
2021-07-14 23:06:42 +0400

    refactor(*): change image

--- pages/about.js
+++ pages/about.js
@@ -29,7 +29,7 @@ export default function About() {
             width="800"
             height="600"
             alt="Vrezh Oganisyan"
-            src="/images/about-avatar.jpg"
+            src="/images/about.jpg"
           />
         </div>
         <div className="mb-4 prose leading-6 text-gray-600 dark:text-gray-400">
