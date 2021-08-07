503fd4a4dce6bd4b8230260d0637df21c044d5f5
vre2h <hovanisyan.vrezh@gmail.com>
2021-08-07 22:51:38 +0400

    fixed prettier running

--- components/BasicLink.jsx
+++ components/BasicLink.jsx
@@ -1,27 +1,23 @@
-import React from 'react';
+import React from "react";
 import cn from "classnames";
 
-const BasicLink = React.forwardRef(({
-  children,
-  className,
-  href,
-  download,
-  ...props
-}, ref) => {
-  return (
-    <a
-    ref={ref}
-      className={cn(
-        className,
-        "cursor-pointer transition duration-500 text-sm text-gray-900 hover:text-gray-900 dark:text-gray-100 border-b-2 p-b-4 dark:border-gray-700 dark:hover:border-gray-100 border-gray-300 hover:border-gray-900"
-      )}
-      href={href}
-      download={download}
-      {...props}
-    >
-      {children}
-    </a>
-  );
-})
+const BasicLink = React.forwardRef(
+  ({ children, className, href, download, ...props }, ref) => {
+    return (
+      <a
+        ref={ref}
+        className={cn(
+          className,
+          "cursor-pointer transition duration-500 text-sm text-gray-900 hover:text-gray-900 dark:text-gray-100 border-b-2 p-b-4 dark:border-gray-700 dark:hover:border-gray-100 border-gray-300 hover:border-gray-900"
+        )}
+        href={href}
+        download={download}
+        {...props}
+      >
+        {children}
+      </a>
+    );
+  }
+);
 
-export default BasicLink
+export default BasicLink;
