a67a2f4b76a11b162e6600621efec160405da4c1
vre2h <hovanisyan.vrezh@gmail.com>
2022-03-11 15:18:43 +0400

    add cv

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ module.exports = {
     locales: ["en-US", "am"],
     defaultLocale: "en-US",
   },
+  async redirects() {
+    return [
+      {
+        source: "/cv",
+        destination:
+          "https://vrezh.notion.site/Oganisyan-Vrezh-CV-47932133fefb45e8aa4b564572a12d87",
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
 };
